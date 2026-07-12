add4706a00368ecfdd74d8e21ce3d0e4396df0f6
MatveyShemiakin <123888755+MatveyShemiakin@users.noreply.github.com>
2026-07-12 18:14:39 +0300

    Bust science section script cache

--- site.js
+++ site.js
@@ -176,10 +176,10 @@
   render(currentLanguage);
 
   const scienceDataScript = document.createElement('script');
-  scienceDataScript.src = '/science-section-data.js?v=20260712-3';
+  scienceDataScript.src = '/science-section-data.js?v=20260712-4';
   scienceDataScript.onload = () => {
     const scienceScript = document.createElement('script');
-    scienceScript.src = '/science-section.js?v=20260712-8';
+    scienceScript.src = '/science-section.js?v=20260712-9';
     document.body.appendChild(scienceScript);
   };
   document.body.appendChild(scienceDataScript);
